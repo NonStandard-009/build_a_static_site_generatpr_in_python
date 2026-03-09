a4ca9158b9e3c7993529fb1ff80395a7df6be8ae
NonStandard-009 <sebaslr.2002@proton.me>
2026-03-09 16:15:14 -0500

    Added tests for NodeClasses

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,14 +1,58 @@
-from htmlnode import *
+import unittest
+from htmlnode import HTMLNode
 
 
-def main():
-    props = {
+class TestHTMLNode(unittest.TestCase):
+    def test_props_output(self):
+        props = {
             "href": "https://www.google.com",
             "target": "_blank",
             }
-    node = HTMLNode(props=props) 
-    print(node.__repr__())
+        node = HTMLNode(
+                tag="<b>",
+                value="This is the text inside this node.",
+                props={
+                    "href": "https://www.google.com",
+                    "target": "_blank",
+                    }
+                )
+        self.assertEqual(node.props, props)
+
+    def test_eq_tag(self):
+        node = HTMLNode(
+                tag="<b>",
+                value="This is the text inside this node.",
+                )
+        node2 = HTMLNode(
+                tag="<b>",
+                value="This is the text inside this node.",
+                )
+        node3 = HTMLNode(
+                tag="<a>",
+                value="This is the text inside this node.",
+                )
+
+        self.assertEqual(node.tag, node2.tag)
+        self.assertNotEqual(node.tag, node3.tag)
+
+    def test_noteq_value(self):
+        node = HTMLNode(
+                tag="<b>",
+                value="This is the text inside this node.",
+                )
+        node2 = HTMLNode(
+                tag="<b>",
+                value="This is the text inside this node.",
+                )
+        node3 = HTMLNode(
+                tag="<a>",
+                value="This is the link for this node.",
+                )
+
+        self.assertNotEqual(node.value, node3.value)
+        self.assertNotEqual(node2.value, node3.value)
+
 
 if __name__ == "__main__":
-    main()
+    unittest.main()
 
