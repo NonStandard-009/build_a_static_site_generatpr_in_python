ddcfd337cdac27e8700243d6770b82ef4698475b
NonStandard-009 <sebaslr.2002@proton.me>
2026-03-10 17:04:56 -0500

    Added test for split_node function

--- src/test_split_nodes.py
+++ src/test_split_nodes.py
@@ -19,3 +19,37 @@ class TestSplitNodes(unittest.TestCase):
 
         self.assertEqual(new_nodes, test_case[1])
 
+    def test_bold_text(self):
+        test_case = [
+                TextNode("This is text with a **bold text** word, here is another `code block` word.", TextType.TEXT), 
+                [
+                    TextNode("This is text with a ", TextType.TEXT),
+                    TextNode("bold text", TextType.BOLD),
+                    TextNode(" word, here is another `code block` word.", TextType.TEXT),
+                    ]
+                ]
+        new_nodes = split_nodes_delimiter([test_case[0]], "**", TextType.BOLD)
+
+        self.assertEqual(new_nodes, test_case[1])
+
+    def test_bold_code_text(self):
+        test_case = [
+                TextNode("This is text with a **bold text** word, here is another `code block` word.", TextType.TEXT), 
+                [
+                    TextNode("This is text with a ", TextType.TEXT),
+                    TextNode("bold text", TextType.BOLD),
+                    TextNode(" word, here is another ", TextType.TEXT),
+                    TextNode("code block", TextType.CODE),
+                    TextNode(" word.", TextType.TEXT)
+                    ]
+                ]
+        new_nodes = split_nodes_delimiter([test_case[0]], "**", TextType.BOLD)
+        new_nodes = split_nodes_delimiter(new_nodes, "`", TextType.CODE)
+
+        self.assertEqual(new_nodes, test_case[1])
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
+
