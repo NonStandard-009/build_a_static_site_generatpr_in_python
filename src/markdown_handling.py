e7edd8ea34124a4890f063f81ac16e4c5a2a1ca0
NonStandard-009 <sebaslr.2002@proton.me>
2026-03-11 16:11:14 -0500

    Started logic for markdown processing

--- src/markdown_handling.py
+++ src/markdown_handling.py
@@ -1,3 +1,39 @@
+from enum import Enum
+
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED_LIST = "unordered_list"
+    ORDERED_LIST = "ordered_list"
+
+
+def block_to_block_type(block) -> BlockType:
+    if block.startswith('#'):
+        counter = 0
+        for chr in block[:6]:
+            if chr != '#':
+                break
+            counter += 1
+        if 1 <= counter <= 6 and block[counter] == ' ':
+            return BlockType.HEADING
+    
+    elif block.startswith("```\n") and block.endswith("```"):
+        return BlockType.CODE
+    
+    elif block.startswith('>') or block.startswith('> '):
+        return BlockType.QUOTE
+    
+    elif block.startswith("- "):
+        return BlockType.UNORDERED_LIST
+    
+    elif block.startswith("1. "):
+        return BlockType.ORDERED_LIST
+    
+    return BlockType.PARAGRAPH
+
 def markdown_to_blocks(markdown):
     md_blocks = []
 
@@ -23,6 +59,18 @@ This is the same paragraph on a new line
 
     for block in md_blocks:
         print(block)
+
+    testing_blocks = [
+            "# This # is a heading",
+            "```\n if {This test block is code}:\n\tThe output should be = BlockType.CODE```",
+            "> This is quoting Boots, a quote...",
+            "- This is an item on a list\n- This is another item",
+            "1. This is number one\n2. This is number two",
+            "This is just lazy"
+            ]
+    
+    for block in testing_blocks:
+        print(block_to_block_type(block))
 
 
 if __name__ == "__main__":
--- src/test_markdown_handling.py
+++ src/test_markdown_handling.py
@@ -1,5 +1,5 @@
 import unittest
-from markdown_handling import markdown_to_blocks
+from markdown_handling import BlockType, markdown_to_blocks, block_to_block_type
 
 
 class TestMarkdownHandling(unittest.TestCase):
@@ -22,6 +22,33 @@ This is the same paragraph on a new line
                 "- This is a list\n- with items",
             ],
         )
+
+    def test_block_to_block_type(self):
+        resulting_types = []
+        testing_blocks = [
+            "# This # is a heading",
+            "```\n if {This test block is code}:\n\tThe output should be = BlockType.CODE```",
+            "> This is quoting Boots, a quote...",
+            "- This is an item on a list\n- This is another item",
+            "1. This is number one\n2. This is number two",
+            "This is just lazy"
+            ]
+        
+        for block in testing_blocks:
+            resulting_types.append(block_to_block_type(block))
+
+
+        self.assertListEqual(
+                resulting_types,
+                [
+                    BlockType.HEADING,
+                    BlockType.CODE,
+                    BlockType.QUOTE,
+                    BlockType.UNORDERED_LIST,
+                    BlockType.ORDERED_LIST,
+                    BlockType.PARAGRAPH
+                    ]
+                )
 
 
 if __name__ == "__main__":
