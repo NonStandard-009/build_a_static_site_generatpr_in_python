3f2a7617a94266596fbab4fad10b638e2ae2e47e
NonStandard-009 <sebaslr.2002@proton.me>
2026-03-10 19:05:34 -0500

    Added spliting for images and links

--- src/split_nodes.py
+++ src/split_nodes.py
@@ -1,4 +1,5 @@
 from textnode import TextType, TextNode
+from extract_from_markdown import extract_markdown_images, extract_markdown_links
 
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type) -> list:
@@ -23,17 +24,70 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type) -> list:
     return new_nodes 
 
 
+def split_nodes_image(old_nodes):
+    new_nodes = []
+
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+            continue
+
+        images_data = extract_markdown_images(node.text)
+        if len(images_data) == 0:
+            new_nodes.append(node)
+            continue
+
+        remaining_text = node.text
+        for image in images_data:
+            before, after = remaining_text.split(f"![{image[0]}]({image[1]})", 1)
+            if before != "":
+                new_nodes.extend([TextNode(before, TextType.TEXT)])
+            new_nodes.extend([TextNode(image[0], TextType.IMAGE, image[1])])
+            remaining_text = after
+        
+        if len(remaining_text) != 0:
+            new_nodes.extend([TextNode(remaining_text, TextType.TEXT)])
+
+    return new_nodes 
+
+
+def split_nodes_link(old_nodes):
+    new_nodes = []
+
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+            continue
+
+        links_data = extract_markdown_links(node.text)
+        if len(links_data) == 0:
+            new_nodes.append(node)
+            continue
+
+        remaining_text = node.text
+        for link in links_data:
+            before, after = remaining_text.split(f"[{link[0]}]({link[1]})", 1)
+            if before != "":
+                new_nodes.extend([TextNode(before, TextType.TEXT)])
+            new_nodes.extend([TextNode(link[0], TextType.LINK, link[1])])
+            remaining_text = after
+        
+        if len(remaining_text) != 0:
+            new_nodes.extend([TextNode(remaining_text, TextType.TEXT)])
+
+    return new_nodes
+
+
 def main():
-    node = TextNode("This is **text** with a `code block`! word. here is another `code block` word", TextType.TEXT)
-
-    new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
-    new_nodes = split_nodes_delimiter(new_nodes, "**", TextType.BOLD)
-
+    node = TextNode(
+        "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)",
+        TextType.TEXT,
+    )
+    new_nodes = split_nodes_link([node])
     
-    print("[")
     for node in new_nodes:
-        print(f"\t {node},")
-    print("]")
+        print(node)
+
 
 if __name__ == "__main__":
     main()
