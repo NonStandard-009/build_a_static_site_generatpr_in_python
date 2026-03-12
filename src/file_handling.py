7e52d01139ac664b1e1e921009400123c6a41acd
NonStandard-009 <sebaslr.2002@proton.me>
2026-03-12 12:44:08 -0500

    Safety copy

---
+++ src/file_handling.py
@@ -0,0 +1,21 @@
+import os, shutil
+
+
+def create_directory(absolute_path):
+    if os.path.exists(absolute_path):
+        shutil.rmtree(absolute_path)
+    os.mkdir(absolute_path)
+
+def copy_files(src_path, dst_path):
+    create_directory(dst_path)
+    static_files = os.listdir(src_path)
+    
+    for item in static_files:
+        if os.path.isfile(item):
+            new_file_path = os.path.join(src_path, item)
+            shutil.copy(new_file_path, dst_path)
+        else:
+            new_src_path = os.path.join(src_path, item)
+            new_dst_path = os.path.join(dst_path, item)
+            copy_files(new_src_path, new_dst_path)
+
--- src/main.py
+++ src/main.py
@@ -1,8 +1,12 @@
-from textnode import *
+import os
+from src.file_handling import copy_files
+
+ABSOLUT_PATH_PUBLIC = os.path.abspath("../public/")
+ABSOLUT_PATH_STATIC = os.path.abspath("../static")
 
 
 def main():
-    pass
+    copy_files(ABSOLUT_PATH_STATIC, ABSOLUT_PATH_PUBLIC)
 
 if __name__ == "__main__":
     main()
